077f07917fee025fc43a290cfa37133d8548ca2f
Caique Sousa <caique_sousa@outlook.com.br>
2017-09-05 03:06:09 -0300

    arq.py

--- Relogio/relogio.py
+++ Relogio/relogio.py
@@ -7,9 +7,12 @@ rel = tk.Label(text = time("%H:%M:%S"), font="Helvetica 120 bold")
 rel.pack()
 
 def relogio():
+    
     agora = time("%H:%M:%S")
     if rel['text'] != agora:
+        
         rel['text'] = agora
+        
     rel.after(100,relogio)
 
 relogio()
